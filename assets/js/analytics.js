c9068e0e03206818ce22c0d870b979f6d5951a9c
Lucia Domenichelli <ldomenichelli2@gmail.com>
2025-09-07 11:08:41 +0200

    feat: show visitor count badge

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -6,6 +6,11 @@
     .then(function (res) { return res.json(); })
     .then(function (res) {
       console.log('Page view:', res.value);
+      var img = document.createElement('img');
+      img.id = 'visitor-counter';
+      img.alt = 'You are visitor number ' + res.value;
+      img.src = 'https://img.shields.io/badge/you%20are%20visitor%20number-' + res.value + '-blue';
+      document.body.appendChild(img);
     })
     .catch(function (err) {
       console.error('CountAPI error:', err);
